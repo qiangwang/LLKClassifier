3323c73e69a67c68d3ed89f9650dfe2c1f5608a3
qiangwang <wangqiang@baixing.com>
2015-05-30 17:19:05 +0800

    ipad->mac

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ llk.add_keyword(u'苹果', u'手机', 'n')
 llk.add_keyword(u'手机', u'手机', 'n')
 llk.add_keyword(u'手机壳', u'手机配件', 'b')
 llk.add_keyword(u'苹果', u'笔记本', 'a')
-llk.add_keyword(u'ipad', u'笔记本', 'n')
+llk.add_keyword(u'mac', u'笔记本', 'n')
 llk.add_keyword(u'苹果', u'农产品', 'n')
 
 #分类测试
@@ -23,4 +23,4 @@ assert llk.classify(u'苹果').keys() == [u'手机', u'农产品']
 assert llk.classify(u'苹果手机') == u'手机'
 assert llk.classify(u'苹果手机壳') == u'手机配件'
 assert llk.classify(u'苹果手机送手机壳') == u'手机'
-assert llk.classify(u'苹果ipad') == u'笔记本'
+assert llk.classify(u'苹果mac') == u'笔记本'
